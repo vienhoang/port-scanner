8b0d47111cdcb68d0c6b8e2df7b3c8ba202d0038
D1skborste <kossan_90@hotmail.com>
2025-10-25 21:08:04 +0200

    e

--- examples/server.py
+++ examples/server.py
@@ -3,7 +3,7 @@ import socket
 import sys
 
 
-def start_server(host="45.33.32.156", port=80):
+def start_server(host="0.0.0.0", port=8080):
     # Create a TCP/IP socket of IPv4 family and SOCK_STREAM type
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # To avoid getting the "Address already in use" error
@@ -53,5 +53,6 @@ def start_server(host="45.33.32.156", port=80):
 
 
 if __name__ == "__main__":
-    start_server()
+    start_server("127.0.0.1", 31337)
+    start_server("127.0.0.2", 31337)
 
